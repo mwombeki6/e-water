53c8d92a48ef48746c51286093cc266cdea6fd8c
mwombeki6 <hackerlubex@protonmail.com>
2024-07-17 11:19:38 +0300

    add auth files

--- app/index.tsx
+++ app/index.tsx
@@ -2,6 +2,7 @@ import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 import { Video, ResizeMode } from "expo-av";
 import React from "react";
 import { useAssets } from "expo-asset";
+import { router } from "expo-router";
 
 export default function Index() {
   const video = React.useRef(null)
@@ -13,15 +14,15 @@ export default function Index() {
         assets && <Video ref={video} style={styles.video} source={{ uri: assets[0].uri }} resizeMode={ResizeMode.COVER} shouldPlay isLooping />
       }
       <View style={styles.overlay}>
-        <Text style={styles.mainText}>e-water</Text>
+        <Text style={styles.mainText}>e~water</Text>
         <Text style={styles.subText}>electronic water</Text>
         <Text style={styles.subText}>Fetch water, fetch electronic</Text>
       </View>
       <View style={styles.buttons}>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button} onPress={() => router.push("/login")}>
           <Text style={styles.buttonText}>Login</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button} onPress={() => router.push("/register")}>
           <Text style={styles.buttonText}>Register</Text>
         </TouchableOpacity>
       </View>
