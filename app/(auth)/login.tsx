394aed093145a1cfcd0f99b37b7d2cff03ca4377
mwombeki6 <hackerlubex@protonmail.com>
2024-07-17 13:12:44 +0300

    commit

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,5 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Formik } from 'formik'
+import * as Yup from 'yup'
+
+//Schema
+const validationShema = Yup.object().shape({
+    email: Yup.string()
+})
 
 export default function login() {
   return (
