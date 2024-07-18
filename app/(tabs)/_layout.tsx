52483609389b5f1d2adc709adc883dfeaa1ec266
mwombeki6 <hackerlubex@protonmail.com>
2024-07-18 16:54:23 +0300

    commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,40 @@
 import { Tabs } from 'expo-router';
+import { FontAwesome } from '@expo/vector-icons';
 
 export default function TabLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="profile" />
-      <Tabs.Screen name="dispense" />
+      <Tabs.Screen
+        name="index"
+        options={{
+          headerShown: false,
+
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="home" color={color} />
+          ),
+        }}
+      />
+      {/* profile */}
+
+      <Tabs.Screen
+        name="dispense"
+        options={{
+          title: "Dispense",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="user" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome size={28} name="cog" color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
